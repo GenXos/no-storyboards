f58e6e9fc96009c88c0c016eecc87107fa16145d
Todd Fields <toddwfields@gmail.com>
2015-12-02 17:50:32 -0600

    first view controller with graphics and button

---
+++ NoStoryboards/ArmyViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ArmyViewController.swift
+//  NoStoryboards
+//
+//  Created by Todd Fields on 2015-12-02.
+//  Copyright © 2015 Todd Fields. All rights reserved.
+//
+
+import UIKit
+
+class ArmyViewController: UIViewController {
+
+ 
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+
+}
